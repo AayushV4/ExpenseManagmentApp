f4e144bc7056d6a628cbe76186f3497d849ad4d2
AayushV4 <vispute_aayush@hotmail.com>
2023-04-08 17:39:51 -0400

    New Category and Expense
    Add Category ; Add Expense ; Delete Expenses

--- ExpenseApp/website/models.py
+++ ExpenseApp/website/models.py
@@ -1,7 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
+from datetime import datetime
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +16,17 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    expenses = db.relationship('Expense', backref='category', lazy=True)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    
--- ExpenseApp/website/views.py
+++ ExpenseApp/website/views.py
@@ -1,23 +1,21 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, User, Category, Expense
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
+        note = request.form.get('note')
         if len(note) < 1:
             flash('Note is too short!', category='error') 
         else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
 
@@ -25,8 +23,8 @@ def home():
 
 
 @views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+def delete_note():
+    note = json.loads(request.data)
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
@@ -35,3 +33,54 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+@views.route('/categories', methods=['GET', 'POST'])
+@login_required
+def categories():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        user_id = current_user.id
+        if not name:
+            flash('Name is required!', category='error')
+        else:
+            category = Category(name=name, user_id=user_id) 
+            db.session.add(category)
+            db.session.commit()
+            flash('Category created!', category='success')
+
+    categories = Category.query.all()
+    return render_template('categories.html', user=current_user, categories=categories)
+
+@views.route('/expenses', methods=['GET', 'POST'])
+@login_required
+def expenses():
+    if request.method == 'POST':
+        amount = request.form.get('amount')
+        category_id = request.form.get('category')
+
+        if not amount:
+            flash('Amount is required!', category='error')
+        elif not category_id:
+            flash('Category is required!', category='error')
+        else:
+            expense = Expense(amount=amount, category_id=category_id, user_id=current_user.id)
+            db.session.add(expense)
+            db.session.commit()
+            flash('Expense created!', category='success')
+
+    categories = Category.query.all()
+    expenses = Expense.query.filter_by(user_id=current_user.id).all()
+    return render_template('expenses.html', user=current_user, categories=categories, expenses=expenses)
+
+
+@views.route('/expenses/delete/<int:expense_id>', methods=['POST'])
+@login_required
+def delete_expense(expense_id):
+    expense = Expense.query.filter_by(id=expense_id, user_id=current_user.id).first()
+    if expense:
+        db.session.delete(expense)
+        db.session.commit()
+        flash('Expense deleted!', category='success')
+    else:
+        flash('Expense not found!', category='error')
+    return redirect(url_for('views.expenses'))
